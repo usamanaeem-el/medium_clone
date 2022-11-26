be71bca349e7e5f3324a91b6ea4af1aedb578142
Usama Naeem <usama.naeem@emerald-labs.com>
2022-11-26 22:02:24 +0500

    App setup and configured tailwind css Created Navbar and Hero section

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../shared/assests/logo.png';
+const Navbar = () => {
+  return (
+    <nav className='h-16 flex justify-between bg-yellow items-center px-8 md:px-12 xl:px-60'>
+      <div>
+        <img className='h-12' src={logo} alt='medium' />
+      </div>
+      <div className='flex items-center'>
+        <ul className='hidden md:flex'>
+          <li className='px-3'>Our story</li>
+          <li className='px-3'>Membership</li>
+          <li className='px-3'>Write</li>
+          <li className='px-5'>Sign In</li>
+        </ul>
+        <button className='text-textWhite px-3 rounded-full bg-pureBlack h-10'>
+          Get Started
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        yellow: '#FFC017',
+        pureBlack: '#000000',
+        pink: '#FFC0CB',
+        textWhite: '#FFFFFF',
+      },
+    },
+    fontFamily: {
+      sans: ['ui-sans-serif', 'system-ui'],
+      roboto: ['Roboto', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
