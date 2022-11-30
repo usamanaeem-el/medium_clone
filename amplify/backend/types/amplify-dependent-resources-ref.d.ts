28aeef6182b4670066cdccfe9f7b6f1b9705236e
Usama Naeem <usama.naeem@emerald-labs.com>
2022-11-30 17:04:55 +0500

    added amplify config

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "mediumclone97fda674": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "adminGroupRole": "string",
+            "consumerGroupRole": "string"
+        }
+    }
+}
